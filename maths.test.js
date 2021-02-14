52a85a9526ad0599d994f39d522fca3461bcae2b
Stephanie Rockett <dr.rockett@Stephanies-MacBook-Pro-2.local>
2021-02-14 12:10:51 -0500

    added new tests and changed some file names

--- maths.test.js
+++ maths.test.js
@@ -5,3 +5,8 @@ test('subtracts 4 - 2 to equal 2', () => {
 test('subtracts 10 - 2 to equal 8', () => {
    expect(subtraction(10, 2)).toBe(8);
 });
+
+const addition = require('./addition');
+test('adds 10 + 2 to equal 12', () => {
+   expect(addition(10, 2)).toBe(12);
+});
