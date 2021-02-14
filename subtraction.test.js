29a3fc84ab51eea82e2d0ce7b936cab7204e262b
Stephanie Rockett <dr.rockett@Stephanies-MacBook-Pro-2.local>
2021-02-14 11:54:00 -0500

    added another test

--- subtraction.test.js
+++ subtraction.test.js
@@ -2,3 +2,8 @@ const subtraction = require('./subtraction');
 test('subtracts 4 - 2 to equal 2', () => {
    expect(subtraction(4, 2)).toBe(2);
 });
+
+const subtraction = require('./subtraction');
+test('subtracts 10 - 2 to equal 8', () => {
+   expect(subtraction(10, 2)).toBe(8);
+});
